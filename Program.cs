cde9285138e89e3bdb3b2840dae591f8ebc6e35e
gangsir <gangsir@outlook.com>
2014-11-10 16:43:47 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
